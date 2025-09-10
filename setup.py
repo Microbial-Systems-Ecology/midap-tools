d0985996f63983636ad904c535c1d04150cd2178
Lukas von Ziegler <lukasv@ethz.ch>
2025-09-10 10:36:04 +0200

    fixed dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements():
         "statsmodels",
         "pytest",
         "scikit-learn",
-        "scikit-image"
+        "scikit-image",
         "plotnine",
         "seaborn",
         "imageio"
